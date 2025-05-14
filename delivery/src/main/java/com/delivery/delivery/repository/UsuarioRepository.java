a986d51650f01211941146523ec179d37581347c
ruan <ruansouzabressan08@gmail.com>
2025-05-14 17:01:26 -0300

    Criado Autenticação com Security

---
+++ delivery/src/main/java/com/delivery/delivery/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.delivery.delivery.repository;
+
+import com.delivery.delivery.entity.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
+    Optional<UsuarioEntity> findByDsEmail(String dsEmail);
+}
+
+
