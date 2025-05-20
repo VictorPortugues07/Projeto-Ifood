3491ae0fcae156dc24832bdbc4fde498d94ecfa2
joaoo737 <joaohbs061107@gmail.com>
2025-05-20 16:27:49 -0300

    Instalação de Dependências + Navbar

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import React from 'react';
+import Navbar from './Navbar';
 import CadastroCliente from './pages/CadastroCliente'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <CadastroCliente/>
     </div>
   );
